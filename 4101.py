1cf8ca8d78a4bce49bbaed415bcbeaab0aaa705f
KoYoungin <2019320054@korea.ac.kr>
2024-07-03 10:25:08 +0900

    24.7.3 no.4101

---
+++ 4101.py
@@ -0,0 +1,13 @@
+while 1:
+    try:
+        a,b = map(int,input().split())
+        if a==0 and b==0:
+            break
+        if a>b:
+            print("Yes")
+        else:
+            print("No")
+    except EOFError:
+        break
+    except ValueError:
+        continue
