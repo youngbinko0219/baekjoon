7e78a91bda52d961654b446b569923d2c9103684
KoYoungin <2019320054@korea.ac.kr>
2024-07-05 17:49:57 +0900

    24.7.5 no.30676

---
+++ 30676.py
@@ -0,0 +1,15 @@
+a = int(input())
+if a>=620 and a<=780:
+    print("Red")
+elif a>=590 and a<620:
+    print("Orange")
+elif a>=570 and a<590:
+    print("Yellow")
+elif a>=495 and a<570:
+    print("Green")
+elif a>=450 and a<495:
+    print("Blue")
+elif a>=425 and a<450:
+    print("Indigo")
+elif a>=380 and a<425:
+    print("Violet")
