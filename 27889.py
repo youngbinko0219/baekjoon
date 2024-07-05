8b849e0bd3aeaa6d0115b64370dd45498668eba4
KoYoungin <2019320054@korea.ac.kr>
2024-07-05 13:16:32 +0900

    24.7.5 no.27889

---
+++ 27889.py
@@ -0,0 +1,9 @@
+a = input()
+if a=="NLCS":
+    print("North London Collegiate School")
+elif a=="BHA":
+    print("Branksome Hall Asia")
+elif a=="KIS":
+    print("Korea International School")
+elif a=="SJA":
+    print("St. Johnsbury Academy")
