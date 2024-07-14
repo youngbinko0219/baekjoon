9e12e065caa22ecc24e89dd098c5b1acd218b145
KoYoungin <2019320054@korea.ac.kr>
2024-07-14 22:00:14 +0900

    24.7.14 no.25377

---
+++ 25377.py
@@ -0,0 +1,27 @@
+from sys import stdin
+n = int(stdin.readline().strip())
+result = 1001
+for i in range(n):
+    a,b = map(int,stdin.readline().split())
+    if b>=a:
+        if b<result:
+            result = b
+if result == 1001:
+    print(-1)
+else:
+    print(result)
+
+# n = int(input().strip())
+
+# result = 1001
+
+# for i in range(n):
+#     a, b = map(int, input().split())
+#     if b >= a:
+#         if b < result:
+#             result = b
+
+# if result == 1001:
+#     print(-1)
+# else:
+#     print(result)
